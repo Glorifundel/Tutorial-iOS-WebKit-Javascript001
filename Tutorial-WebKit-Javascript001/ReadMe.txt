1d7d5e8d638af5da429981000fa67c6b9915beb2
Kevin Quinn <KQUINN@macbookpro-fbaf.meditech.com>
2016-02-05 14:59:56 -0500

    Functional Example of a simple WebKit using a local index.html and main.js file

---
+++ Tutorial-WebKit-Javascript001/ReadMe.txt
@@ -0,0 +1,34 @@
+Created by Kevin Quinn on 20160205.
+Copyright © 2016 Kevin Quinn. All rights reserved.
+
+//--------------------------------------------------
+// MARK: Setup - Project Settings
+//--------------------------------------------------
+General
+    Linked Frameworks and Libraries
+        + WebKit.framework
+
+//--------------------------------------------------
+// MARK: Setup - Info.plist
+//--------------------------------------------------
+Information Property List
+    + App Transport Security Settings
+        + Allow Arbitrary Loads
+            YES    //Allows network requests to unsecured protocols (http)
+
+//--------------------------------------------------
+// MARK: Documentation - Internal References
+//--------------------------------------------------
+<none>
+
+//--------------------------------------------------
+// MARK: Documentation - External References
+//--------------------------------------------------
+This example code was based on the following:
+
+Kinderas (non-functional in swift2.0)
+http://www.kinderas.com/technology/2014/6/7/getting-started-with-wkwebview-using-swift-in-ios-8
+http://www.kinderas.com/technology/2014/6/15/wkwebview-and-javascript-in-ios-8-using-swift
+
+liqichao's update to the above for swift 2.0
+https://github.com/liqichao/wkwebview-test
--- Tutorial-WebKit-Javascript001/ViewController.swift
+++ Tutorial-WebKit-Javascript001/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler{
 
+    //--------------------------------------------------
+    // MARK: Properties
+    //--------------------------------------------------
+    @IBOutlet var parentView: UIView! = nil
+    
+    var wkWebView: WKWebView?
+    
+    //  Example of listening in ios for a message fired from js
+    //  in main.js:
+    //              webkit.messageHandlers.jsEventHandler.postMessage("Hello from JavaScript");
+    let jsMessageHandler = "jsEventHandler"
+    //
+    //  Example of calling a js function from ios
+    let jsFunction = "redHeader()"
+    
+    //--------------------------------------------------
+    // MARK: Functions
+    //--------------------------------------------------
+    
+    override func loadView() {
+        super.loadView()
+        
+        let wkContentController = WKUserContentController();
+        let jsScript = WKUserScript(source: jsFunction, injectionTime: WKUserScriptInjectionTime.AtDocumentEnd, forMainFrameOnly: true)
+        
+        wkContentController.addUserScript(jsScript)
+        wkContentController.addScriptMessageHandler(self, name: jsMessageHandler)
+        
+        let wkConfiguration = WKWebViewConfiguration()
+        wkConfiguration.userContentController = wkContentController
+        
+        self.wkWebView = WKWebView(frame: self.parentView.bounds, configuration: wkConfiguration)
+        self.view = self.wkWebView!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //  load via url
+        //let url = NSURL(string: "https://www.google.com")
+        //
+        //  load via url for http:// requires special App Transport Security permission in info.plist
+        //let url = NSURL(string: "http://www.darknessdescending.com")
+        //let request = NSURLRequest(URL: url!)
+        //self.wkWebView!.loadRequest(request)
+        
+        //  load via file
+        let fileUrl = NSBundle.mainBundle().pathForResource("index", ofType: "html")!
+        let url = NSURL(fileURLWithPath: fileUrl)
+        self.wkWebView!.loadFileURL(url, allowingReadAccessToURL: url)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //--------------------------------------------------
+    // MARK: Protocols
+    //--------------------------------------------------
+    
+    //WKScriptMessageHandler
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        if (message.name == jsMessageHandler) {
+            print("ViewController.userContentController() - Javascript event has been received:\n\(message.body)")
+        }
     }
-
 
 }
 
